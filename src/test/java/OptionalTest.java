bf0aaec5a3f2e612a47feac7bdc3eaa850135b19
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-11-08 17:01:50 +0100

    isEmpty tests

--- src/test/java/OptionalTest.java
+++ src/test/java/OptionalTest.java
@@ -2,6 +2,9 @@ import org.junit.Test;
 
 import java.util.Optional;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by mtumilowicz on 2018-11-08.
  */
@@ -29,5 +32,15 @@ public class OptionalTest {
     @Test
     public void ifPresentOrElse_optionalNotEmpty() {
         Optional.of("10").ifPresentOrElse(Renderer::page, Renderer::empty);
+    }
+    
+    @Test
+    public void isEmpty_empty() {
+        assertTrue(Optional.empty().isEmpty());
+    }
+
+    @Test
+    public void isEmpty_notEmpty() {
+        assertFalse(Optional.of("not empty").isEmpty());
     }
 }
